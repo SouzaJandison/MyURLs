1a00dceb5369647e1c56539c70999052f1179f98
SouzaJandison <jandsonbarbosa.dev@gmail.com>
2021-07-27 13:59:29 -0300

    init: add React and your configuration, TypeScript and Code Lint

---
+++ web/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './styles/reset.css';
+import './styles/global.css';
+
+import Routes from './routes';
+
+export default function App(): JSX.Element {
+  return <Routes />;
+}
