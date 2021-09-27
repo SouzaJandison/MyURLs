e7207b70b4e5540fd454749c4bcc1b4b01d5df00
SouzaJandison <jandsonbarbosa.dev@gmail.com>
2021-09-27 11:44:36 -0300

    chore: add environment variables

--- server/src/@types/env.d.ts
+++ server/src/@types/env.d.ts
@@ -1,15 +1,19 @@
 declare namespace NodeJS {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   export interface ProcessEnv {
+    APP_API_URL: string;
+    APP_WEB_URL: string;
+
     APP_SECRET: string;
+
     POSTGRES_HOST: string;
     POSTGRES_USER: string;
     POSTGRES_PASS: string;
     POSTGRES_NAME: string;
     POSTGRES_NAME_TEST: string;
+
     MAILER_MAIL: string;
     MAILER_USER: string;
     MAILER_PASS: string;
-    URL_MAIL: string;
   }
 }
