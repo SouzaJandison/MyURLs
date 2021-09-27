27ec8f9e8bb8167e00984dc10e9f851c55597fda
SouzaJandison <jandsonbarbosa.dev@gmail.com>
2021-09-27 10:24:55 -0300

    refactor: move config server to file app.ts

--- server/src/shared/infra/http/server.ts
+++ server/src/shared/infra/http/server.ts
@@ -1,37 +1,4 @@
-import 'reflect-metadata';
-import express, { NextFunction, Request, Response } from 'express';
-import 'express-async-errors';
-
-import { AppError } from '../../errors/AppError';
-import createConnection from '../typeorm';
-import { routes } from './routes';
-
-createConnection();
-
-const app = express();
-
-app.use(express.json());
-app.use(routes);
-
-app.use(
-  (
-    err: Error,
-    request: Request,
-    response: Response,
-    _: NextFunction,
-  ): Response => {
-    if (err instanceof AppError) {
-      return response.status(err.statuscode).json({
-        message: err.message,
-      });
-    }
-
-    return response.status(500).json({
-      status: 'Error',
-      message: `Internal server error ${err.message}`,
-    });
-  },
-);
+import { app } from './app';
 
 app.listen(5000, () =>
   console.log('🔥️ Server Started at http://localhost:5000'),
